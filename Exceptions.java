bbcbe604a7d4bac9401678e7438b52721a7f15b9
Tarctic <zainabshah2003@gmail.com>
2023-01-07 13:43:33 +0300

    Add project file

---
+++ Exceptions.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Exceptions {
+
+    // 3️⃣ THIRD
+    // 🔴 Edit the line below to indicate that this method can throw the exception you created
+    // ⬇️------------Edit the code below (approx. 2 words of code)-----------⬇️
+    public static void main(String[] args) {
+    // ⬆️--------------------------------END HERE------------------------------⬆️
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your email address: ");
+        String email = sc.nextLine();
+        sc.close();
+
+        // 2️⃣ SECOND
+        // 🔴 Check if the String email is a valid email
+        // 🔴 If it is not valid, throw the custom exception that you created and pass the message "Your email is not valid!" as an argument
+        // 🔴 If it is valid, simply print "Your email is valid!"
+        // ⬇️------------Write your code below (approx. 4 lines of code)-----------⬇️
+        
+
+
+        // ⬆️--------------------------------END HERE------------------------------⬆️
+    }
+
+}
+
+// 1️⃣ FIRST
+// 🔴 Create a class for your custom exception
+// 🔴 Create a constructor that will print any message passed to it
+// ⬇️------------Write your code below (approx. 3 lines of code)-----------⬇️
+        
+
+
+
+// ⬆️--------------------------------END HERE------------------------------⬆️
